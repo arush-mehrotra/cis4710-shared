80cd8aa01cd33154a11c70dcb2ea6b42704e36a1
Arush Mehrotra <43364437+arush-mehrotra@users.noreply.github.com>
2024-02-01 03:06:58 +0000

    first attempt

--- hw2a/divider_unsigned.sv
+++ hw2a/divider_unsigned.sv
@@ -12,6 +12,24 @@ module divider_unsigned (
 );
 
     // TODO: your code here
+    wire[31:0] temp_remainder[33];
+    wire[31:0] temp_quotient[33];
+    wire[31:0] temp_dividend[33];
+
+    assign temp_remainder[0] = 32'b0;
+    assign temp_quotient[0] = 32'b0;
+    assign temp_dividend[0] = i_dividend[31:0];
+
+    genvar i;
+        generate for (i = 0; i < 32; i++) begin: g_loop
+            divu_1iter d1(.i_dividend(temp_dividend[i]), .i_divisor(i_divisor[31:0]),
+            .i_remainder(temp_remainder[i]), .i_quotient(temp_quotient[i]),
+            .o_dividend(temp_dividend[i+1]), .o_remainder(temp_remainder[i+1]),
+            .o_quotient(temp_quotient[i+1]));
+        end endgenerate
+
+    assign o_remainder[31:0] = temp_remainder[32];
+    assign o_quotient[31:0] = temp_dividend[32];
 
 endmodule
 
@@ -39,5 +57,11 @@ module divu_1iter (
     */
 
     // TODO: your code here
+    wire [31:0] temp_remainder;
+    assign temp_remainder[31:0] = (i_remainder << 1) | ((i_dividend >> 31) & 32'b1);
+    assign o_quotient[31:0] = (temp_remainder < i_divisor) ? (i_quotient << 1) : ((i_quotient << 1) | 32'b1);
+    assign o_remainder[31:0] = (temp_remainder < i_divisor) ? temp_remainder : (temp_remainder - i_divisor);
+    assign o_dividend[31:0] = i_dividend << 1;
+
 
 endmodule
